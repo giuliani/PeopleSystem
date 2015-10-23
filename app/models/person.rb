821dc25b647b5f3acef3fdcd879b81e20ae18f94
Giuliani Perry <perryg.88@gmail.com>
2015-10-23 16:05:43 -0600

    adding . symbol to email

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,9 +6,9 @@ class Person < ActiveRecord::Base
   validates :last_name, presence: true, length: { maximum: 75 }
   validates :birthdate, presence: true
   validates :email, presence: true, length: { maximum: 254 }, uniqueness: { case_sensitive: false }
-  # valid email contains word chars, # - ~ ! $ & ' ( ) * + , ; = : and percentile encoding i.e. %20
+  # valid email contains word chars, # - ~ ! $ & ' ( ) * + , ; = : . and percentile encoding i.e. %20
   # followed by one @, more than one word char and one . with more than one word character
-  validates_format_of :email, :with => /\A([\w#(%\h{2})\(\)\*\+,;=:'&$!~-])+@{1}\w+.{1}\w+\Z/, :on => :create
+  validates_format_of :email, :with => /\A([\w#(%\h{2})\(\)\*\+\.,;=:'&$!~-])+@{1}\w+.{1}\w+\Z/, :on => :create
   validates_inclusion_of :gender, :in => %w( m f u )
   validate :birthdate_after_today
   validates_format_of :picture, :with => URI.regexp
