1d199e3e940e9512ba495f795ab66f2c9fbb2930
Giuliani Perry <perryg.88@gmail.com>
2015-10-24 14:57:23 -0600

    adding allow blank for picture and gender

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,9 +9,9 @@ class Person < ActiveRecord::Base
   # valid email contains word chars, # - ~ ! $ & ' ( ) * + , ; = : . and percentile encoding i.e. %20
   # followed by one @, more than one word char and one . with more than one word character
   validates_format_of :email, :with => /\A([\w#(%\h{2})\(\)\*\+\.,;=:'&$!~-])+@{1}\w+.{1}\w+\Z/, :on => :create
-  validates_inclusion_of :gender, :in => %w( m f u )
+  validates_inclusion_of :gender, :in => %w( m f u ), allow_blank: true
   validate :birthdate_after_today
-  validates_format_of :picture, :with => URI.regexp
+  validates_format_of :picture, :with => URI.regexp, allow_blank: true
 
   def age
     now = Time.now.utc.to_date
