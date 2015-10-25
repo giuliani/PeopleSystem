f51c06e0700eb635f8caec121da5e07511d815ab
Giuliani Perry <perryg.88@gmail.com>
2015-10-24 23:52:10 -0600

    fixing date range for random birthdate in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 20.times do
-  birthdate = rand(Date.civil(1960, 1, 1)..Date.civil(2015, 12, 31))
+  birthdate = rand(Date.civil(1960, 1, 1)..Date.civil(2014, 12, 31))
   icon = "http://www.asthmamd.org/images/icon_user_1.png"
   bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
   first_name = %w( Andrea Tori Dani Germaine Reese Jaden Angel Jesse Ariel Alex Cameron ).sample
