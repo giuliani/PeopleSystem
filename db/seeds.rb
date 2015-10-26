41455061b0ca6a27d0d5b52bd6c4ed63f1320437
Giuliani Perry <perryg.88@gmail.com>
2015-10-25 20:27:13 -0600

    changing links to 'cancel' in edit and show and creating up to 26 users from seeds to account for duplicate email errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-20.times do
+26.times do
   birthdate = rand(Date.civil(1960, 1, 1)..Date.civil(2014, 12, 31))
   icon = "http://www.asthmamd.org/images/icon_user_1.png"
   bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
