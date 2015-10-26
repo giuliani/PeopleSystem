926d32a1649bcfa2b23090eed80a16c7669c9cab
Giuliani Perry <perryg.88@gmail.com>
2015-10-26 17:12:33 -0600

    adding tomdoc comments for the mailers and workers

--- app/workers/created_person_mailer.rb
+++ app/workers/created_person_mailer.rb
@@ -1,6 +1,16 @@
 class CreatedPersonMailer
   @queue = :person_created_queue
 
+  # Internal: The worker's actions to perform. When the job executes, it calls the PersonMailer
+  #           function to send out the corresponding email.
+  #
+  # queue_hash - the hash containing the created person's person_id and the recipient_id
+  #
+  # Examples
+  #
+  #   CreatedPersonMailer.perform
+  #
+  # Returns nothing. Calls the PersonMailer to send out emails.
   def self.perform queue_hash
     puts "Preparing to send New Person Created Email."
     person = Person.find queue_hash['person_id']
--- app/workers/deleted_person_mailer.rb
+++ app/workers/deleted_person_mailer.rb
@@ -1,6 +1,16 @@
 class DeletedPersonMailer
   @queue = :person_deleted_queue
 
+  # Internal: The worker's actions to perform. When the job executes, it calls the PersonMailer
+  #           function to send out the corresponding email.
+  #
+  # queue_hash - the hash containing the created person's information hash and the recipient_id
+  #
+  # Examples
+  #
+  #   DeletedPersonMailer.perform
+  #
+  # Returns nothing. Calls the PersonMailer to send out emails.
   def self.perform queue_hash
     puts "Preparing to send Person Deleted Email."
     recipient = Person.find queue_hash['recipient_id']
